35668bcec91cefd8f9c8a6813b9ac8c5475e081e
Iwo Szczepaniak <iszczepaniak@student.agh.edu.pl>
2024-03-10 16:44:16 +0100

    lab_gniazda

--- lab_gniazda/PythonUdpClient.py
+++ lab_gniazda/PythonUdpClient.py
@@ -1,13 +1,9 @@
-import socket;
+import socket
 
 serverIP = "127.0.0.1"
 serverPort = 9008
 msg = "Ping Python: Żółta gęś"
 
-print('PYTHON UDP CLIENT')
+print("PYTHON UDP CLIENT")
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(bytes(msg, 'utf-8'), (serverIP, serverPort))
-
-
-
-
+client.sendto(bytes(msg, "utf-8"), (serverIP, serverPort))
